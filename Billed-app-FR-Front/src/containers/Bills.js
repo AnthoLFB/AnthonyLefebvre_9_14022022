1d1057ce626bf5a91304d9d65b6e6912001eebe4
Anthony LEFEBVRE <antho.lefebvre@outlook.fr>
2022-02-27 18:04:17 +0100

    Modification de la méthode getBills(). Ajout de sort() afin d'afficher les elements dans l'ordre decroissant. Attention ne passe pas le test pour le momnent.

--- Billed-app-FR-Front/src/containers/Bills.js
+++ Billed-app-FR-Front/src/containers/Bills.js
@@ -34,6 +34,7 @@ export default class {
       .list()
       .then(snapshot => {
         const bills = snapshot
+          .sort((a,b) => new Date(b.date) - new Date(a.date))
           .map(doc => {
             try {
               return {
