33bbceffcba6265e391fc9a9c82668c64eadc612
Anthony LEFEBVRE <antho.lefebvre@outlook.fr>
2022-03-04 10:26:27 +0100

    Ajout de la mention expect. Cette mention permet de vérifier le test suivant : Je suis connecté en tant qu'employé, quand je suis sur la page Bills, l'icone affichée doit être mise en avant. Deux choses sont testées : L'icone doit exister, et la classe permettant de la mettre en avant doit exister également.

--- Billed-app-FR-Front/src/__tests__/Bills.js
+++ Billed-app-FR-Front/src/__tests__/Bills.js
@@ -2,11 +2,13 @@
  * @jest-environment jsdom
  */
 
-import {screen, waitFor} from "@testing-library/dom"
-import BillsUI from "../views/BillsUI.js"
-import { bills } from "../fixtures/bills.js"
-import { ROUTES_PATH} from "../constants/routes.js";
+import {screen, waitFor} from "@testing-library/dom";
+import userEvent from '@testing-library/user-event'
+import BillsUI from "../views/BillsUI.js";
+import { bills } from "../fixtures/bills.js";
+import { ROUTES, ROUTES_PATH} from "../constants/routes.js";
 import {localStorageMock} from "../__mocks__/localStorage.js";
+import Bills from "../containers/Bills.js";
 
 import router from "../app/Router.js";
 
@@ -29,7 +31,7 @@ describe("Given I am connected as an employee", () => {
       expect(windowIcon).not.toBeNull();
       expect(windowIcon.classList.contains('active-icon')).toBe(true);
     })
-    test.skip("Then bills should be ordered from earliest to latest", () => {
+    test("Then bills should be ordered from earliest to latest", () => {
       document.body.innerHTML = BillsUI({ data: bills })
       const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
       const antiChrono = (a, b) => ((a < b) ? 1 : -1)
