614cb984c27638736ab7dd8564586b19d8848818
Anthony LEFEBVRE <antho.lefebvre@outlook.fr>
2022-03-13 21:21:40 +0100

    Ajout d'un premier test permettant de vérifier le comportement du code lorsqu'un fichier de type image est inséré. On teste ici le fait que le nom de l'image apparaisse lorsque le fichier est valide.

--- Billed-app-FR-Front/src/__tests__/NewBill.js
+++ Billed-app-FR-Front/src/__tests__/NewBill.js
@@ -2,9 +2,10 @@
  * @jest-environment jsdom
  */
 
-import {screen, waitFor} from "@testing-library/dom";
+import {screen, waitFor, fireEvent} from "@testing-library/dom";
 import {localStorageMock} from "../__mocks__/localStorage.js";
 import router from "../app/Router.js";
+import store from "../__mocks__/store";
 import { ROUTES, ROUTES_PATH} from "../constants/routes.js";
 import NewBillUI from "../views/NewBillUI.js";
 import NewBill from "../containers/NewBill.js";
@@ -30,6 +31,35 @@ describe("Given I am connected as an employee", () => {
       const mailIcon = screen.getByTestId('icon-mail')
       expect(mailIcon).not.toBeNull();
       expect(mailIcon.classList.contains('active-icon')).toBe(true);
+    })
+  })
+  describe("When I am on the form to create a new bill", () => {
+    test.only("Then I fill the field concerning the image with a correct file, so the name of the image should be displayed", async () => {
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee'
+      }))
+      const root = document.createElement("div")
+      root.setAttribute("id", "root")
+      document.body.append(root)
+      router()
+      window.onNavigate(ROUTES_PATH.NewBill)
+
+      const newBill = new NewBill({document, onNavigate, store, localStorage});
+      const handleChangeFile = jest.fn((e) => newBill.handleChangeFile(e));
+      const input = screen.getByTestId("file");
+      input.addEventListener("change", handleChangeFile);
+      //put a good format image (png)
+      fireEvent.change(input, {
+        target: {
+          files: [
+            new File(["image.png"], "image.png", { type: "image/png" }),
+          ],
+        },
+      });
+      //Check if the function is called and the name of the image is good
+      expect(handleChangeFile).toHaveBeenCalled();
+      expect(input.files[0].name).toBe("image.png");
     })
   })
 })
