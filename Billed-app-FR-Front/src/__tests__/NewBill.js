777d10f9ddd813cd7405076bfbc48a35e15b0423
Anthony LEFEBVRE <antho.lefebvre@outlook.fr>
2022-03-13 17:39:17 +0100

    Ajout d'un test permettant de vérifier que l'icone en forme d'enveloppe est mise en avant

--- Billed-app-FR-Front/src/__tests__/NewBill.js
+++ Billed-app-FR-Front/src/__tests__/NewBill.js
@@ -2,17 +2,34 @@
  * @jest-environment jsdom
  */
 
-import { screen } from "@testing-library/dom"
-import NewBillUI from "../views/NewBillUI.js"
-import NewBill from "../containers/NewBill.js"
+import {screen, waitFor} from "@testing-library/dom";
+import {localStorageMock} from "../__mocks__/localStorage.js";
+import router from "../app/Router.js";
+import { ROUTES, ROUTES_PATH} from "../constants/routes.js";
+import NewBillUI from "../views/NewBillUI.js";
+import NewBill from "../containers/NewBill.js";
 
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
-      //to-do write assertion
+    test("Then new bill icon in vertical layout should be highlighted", async () => {
+      //initialisation
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee'
+      }))
+      //html
+      const root = document.createElement("div")
+      root.setAttribute("id", "root")
+      document.body.append(root)
+      //route
+      router()
+      window.onNavigate(ROUTES_PATH.NewBill)
+      //récupération et test
+      await waitFor(() => screen.getByTestId('icon-mail'))
+      const mailIcon = screen.getByTestId('icon-mail')
+      expect(mailIcon).not.toBeNull();
+      expect(mailIcon.classList.contains('active-icon')).toBe(true);
     })
   })
 })
