28680f9b6946b008ed5734d3c7f97809a96b0717
Anthony LEFEBVRE <antho.lefebvre@outlook.fr>
2022-03-27 17:32:15 +0200

    Ajout de trois tests permettant de vérifier le comportement du code lorsqu'un fichier est inséré dans le champ image. Dans un premier temps on teste (avec un fichier au bon format), si la fonction permettant de vérifier le fichier est appelée. Dans un deuxième temps, on teste si le message d'erreur s'affiche bien en cas d'import d'un mauvais format de fichier. Enfin on teste si les message d'erreur ne sont pas dupliqué si l'utilisateur importe plusieurs fois de suite un fichier au mauvais format.

--- Billed-app-FR-Front/src/__tests__/NewBill.js
+++ Billed-app-FR-Front/src/__tests__/NewBill.js
@@ -6,8 +6,7 @@ import {screen, waitFor, fireEvent} from "@testing-library/dom";
 import {localStorageMock} from "../__mocks__/localStorage.js";
 import router from "../app/Router.js";
 import store from "../__mocks__/store";
-import { ROUTES, ROUTES_PATH} from "../constants/routes.js";
-import NewBillUI from "../views/NewBillUI.js";
+import {ROUTES_PATH} from "../constants/routes.js";
 import NewBill from "../containers/NewBill.js";
 
 
@@ -34,7 +33,7 @@ describe("Given I am connected as an employee", () => {
     })
   })
   describe("When I am on the form to create a new bill", () => {
-    test.only("Then I fill the field concerning the image with a correct file, so the name of the image should be displayed", async () => {
+    test("Then I fill the field concerning the image with a correct file, so the name of the image should be displayed", () => {
       Object.defineProperty(window, 'localStorage', { value: localStorageMock })
       window.localStorage.setItem('user', JSON.stringify({
         type: 'Employee'
@@ -44,12 +43,10 @@ describe("Given I am connected as an employee", () => {
       document.body.append(root)
       router()
       window.onNavigate(ROUTES_PATH.NewBill)
-
       const newBill = new NewBill({document, onNavigate, store, localStorage});
       const handleChangeFile = jest.fn((e) => newBill.handleChangeFile(e));
       const input = screen.getByTestId("file");
       input.addEventListener("change", handleChangeFile);
-      //put a good format image (png)
       fireEvent.change(input, {
         target: {
           files: [
@@ -57,9 +54,68 @@ describe("Given I am connected as an employee", () => {
           ],
         },
       });
-      //Check if the function is called and the name of the image is good
       expect(handleChangeFile).toHaveBeenCalled();
       expect(input.files[0].name).toBe("image.png");
+    })
+    test("Then I fill the field concerning the image with a incorrect file, so an error should be displayed", () => {
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee'
+      }))
+      const root = document.createElement("div")
+      root.setAttribute("id", "root")
+      document.body.append(root)
+      router()
+      window.onNavigate(ROUTES_PATH.NewBill)
+      const input = screen.getByTestId('file');
+      fireEvent.change(input, {
+        target: {
+          files: [
+            new File(["test"], "test.txt", { type: "text/plain" }),
+          ],
+        },
+      });
+      let error = document.getElementsByClassName("errorMessage");
+      let errorMessage = error[0].textContent;
+      expect(errorMessage).not.toBeNull();
+      expect(errorMessage).toBe("L'extension de votre fichier n'est pas supportée. Seuls les fichiers JPG, JPEG et PNG sont acceptés.")
+    })
+    test("when an error is displayed, the message must not be duplicated", () => {
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee'
+      }))
+      const root = document.createElement("div")
+      root.setAttribute("id", "root")
+      document.body.append(root)
+      router()
+      window.onNavigate(ROUTES_PATH.NewBill)
+      const input = screen.getByTestId('file');
+      fireEvent.change(input, {
+        target: {
+          files: [
+            new File(["test"], "test.txt", { type: "text/plain" }),
+          ],
+        },
+      });
+      fireEvent.change(input, {
+        target: {
+          files: [
+            new File(["test2"], "test2.txt", { type: "text/plain" }),
+          ],
+        },
+      });
+      fireEvent.change(input, {
+        target: {
+          files: [
+            new File(["test3"], "test3.txt", { type: "text/plain" }),
+          ],
+        },
+      });
+      let error = document.getElementsByClassName("errorMessage");
+      let countErrorMessage = error.length;
+      expect(countErrorMessage).not.toBeNull();
+      expect(countErrorMessage).toBe(1);
     })
   })
 })
